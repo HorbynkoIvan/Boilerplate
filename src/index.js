0356533266954ca49ba28f954578369fa31c6f48
Horbynko Ivan <horbynko.i@gmail.com>
2019-02-20 11:39:29 +0200

    routing from v2 to v4

--- src/index.js
+++ src/index.js
@@ -5,27 +5,33 @@ import ReactDOM from 'react-dom'
 import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import {syncHistoryWithStore} from 'react-router-redux'
-import {Router, Route, BrowserRouter} from 'react-router-dom'
+//import {syncHistoryWithStore} from 'react-router-redux'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 import {Provider} from 'react-redux'
 
 import reducers from 'reducers'
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
+import Error from 'containers/Error'
 
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk)
 ))
 
-const history = syncHistoryWithStore(BrowserRouter, store)
+//const history = syncHistoryWithStore(BrowserRouter, store)
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
-            <Route component={Layout}>
-                <Route path='/' component={Phones} />
-            </Route>
-        </Router>
+        <BrowserRouter>
+            <div>
+                <Layout>
+                    <Switch>
+                        <Route path='/' component={Phones} exact/>
+                        <Route component={Error}/>
+                    </Switch>
+                </Layout>
+            </div>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
